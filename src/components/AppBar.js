6b5b3a48f39e8a0e57aeb350d5b579845c3fc53f
alme8457 <103006458+alme8457@users.noreply.github.com>
2022-11-21 20:17:01 -0500

    changed Heading level in app bar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -10,7 +10,7 @@ export default function ButtonAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
             CLASS2022 Bank, Inc.
           </Typography>
           <Button color="inherit">Login</Button>
