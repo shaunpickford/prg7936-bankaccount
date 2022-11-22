da98387195b659e5577757e91ba2d1933ed3c583
louisse2021 <100004777+louisse2021@users.noreply.github.com>
2022-11-21 20:00:01 -0500

    Change: New Name

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -11,7 +11,7 @@ export default function ButtonAppBar() {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            PRG7936 Bank, Inc.
+            CLASS2022 Bank, Inc.
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
