f21c72ff5f0480425dc8a2ced7bf2141614ce85b
louisse2021 <100004777+louisse2021@users.noreply.github.com>
2022-11-21 20:38:51 -0500

    change bank's name

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -11,7 +11,7 @@ export default function ButtonAppBar() {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-            PRG7936 Bank, Inc.
+            MarieLucie Bank, Inc.
           </Typography>
           <Button color="inherit">Sign out</Button>
         </Toolbar>
