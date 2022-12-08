599bb590bc0dc42d12140652eb68d7da9666c4ed
diarradutton <diarradutton@gmail.com>
2022-12-08 10:59:56 -0500

    Here's my update

--- src/components/Withdrawals.js
+++ src/components/Withdrawals.js
@@ -1,11 +1,13 @@
 import Heading from './Heading';
-
+import {useState} from 'react';
 import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
+import { Button, TextField } from '@mui/material';
 
 // This is a component named Withdrawals
 function Withdrawals(props) {
   // TODO: Track the amount being withdrawn in the state
+  const [withdrawAmount, setWithdrawAmount] = useState(0);
 
 
   // If the amount is less than the balance, user can withdraw
@@ -15,17 +17,36 @@ function Withdrawals(props) {
   /*if (amount > props.balance) {
       canWithdraw = false;
   }*/
+  const onFieldChange = (evt) => {
+    let newAmount = parseInt(evt.target.value);
+  
+  if (newAmount >=0) {
+    setWithdrawAmount(newAmount);
+  }
+}
+
+  const handleWithdraw = () => {
+    props.updateBalance(withdrawAmount);
+    
+    setWithdrawAmount(0);
+  }
 
   return (
     <Box>
       <Heading text="Withdraw Funds" type="h3" />
-
-      {/* If canWithdraw is FALSE, show the Alert */}
-      {canWithdraw === false ? (
-          <Alert severity='warning' sx={{ mt: 2 }}>
-              Insufficient funds for Withdrawal
-          </Alert>
-      ) : null}
+          <TextField
+            type="number"
+            label="Withdraw Amount"
+            value={withdrawAmount}
+            onChange={onFieldChange}
+          />
+          <Button
+            variant="outlined"
+            size="small"
+            onClick={handleWithdraw}
+          >
+              Withdraw
+          </Button>
     </Box>
   );
 }
