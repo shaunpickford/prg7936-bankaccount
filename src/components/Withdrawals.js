d497b8e429d909aa03d3738cb0dfc65a49ad9fb4
alme8457 <103006458+alme8457@users.noreply.github.com>
2022-12-11 15:26:24 -0500

    commit

--- src/components/Withdrawals.js
+++ src/components/Withdrawals.js
@@ -2,11 +2,20 @@ import Heading from './Heading';
 
 import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
+import { useStepContext } from '@mui/material';
 
 // This is a component named Withdrawals
 function Withdrawals(props) {
   // TODO: Track the amount being withdrawn in the state
-
+Const [withdrawAmount, setWithdrawAmount] = useState(0);
+return (
+  <div>
+    <p>You clicked {count} times</p>
+    <button onClick={() => setCount(count + 20)}>
+      Click me
+    </button>
+  </div>
+)
 
   // If the amount is less than the balance, user can withdraw
   
